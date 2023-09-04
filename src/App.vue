32bc2364c674de45e6fe36a961b9068314f586ab
wz <3565087505@qq.com>
2023-09-04 13:49:58 +0800

    初始化vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<!-- 注册在哪，就在哪使用子组件 -->
+  <div>
+
+  </div>
+</template>
+
+<script>
+// 引入子组件
+export default {
+    name:'App',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/School.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+//默认暴露组件
+export default {
+    name:'',
+    data() {
+      return {
+
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
