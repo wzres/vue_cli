a2e7af6350615ec56d92f91e87261b39ab90caba
wz <3565087505@qq.com>
2023-09-29 20:05:20 +0800

    变更

---
+++ src/Pages/GuangDong.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="s1">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'GuangDong'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 // 引入VueRouter
 import VueRouter from 'vue-router'
 // 引入组件
-import HeBei from '../components/HeBei'
-import HeNan from '../components/HeNan'
+import JiangXi from '../Pages/JiangXi'
+import GuangDong from '../components/GuangDong'
 
 // 创建并暴露一个路由器对象
 export default new VueRouter({
