a2fd045041d40f7adadf167394c51a8ae5a8a0ba
wz <3565087505@qq.com>
2023-09-29 21:21:02 +0800

    变更

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,12 @@ import VueRouter from 'vue-router'
 // 引入组件
 import JiangXi from '../Pages/JiangXi'
 import GuangDong from '../components/GuangDong'
+import { component } from 'vue/types/umd'
 
 // 创建并暴露一个路由器对象
 export default new VueRouter({
     routes : [
         // 每一个key对应一个路径，value对应该路径要展示的组件
-        
+        // 注意：对于子路由不要加/
     ]
 })
