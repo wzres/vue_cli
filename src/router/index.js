40dae331d1605e32c19e3213ac8279b5bc000fdb
wz <3565087505@qq.com>
2023-09-30 21:32:21 +0800

    变更

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@
 import VueRouter from 'vue-router'
 // 引入组件
 import JiangXi from '../Pages/JiangXi'
-import GuangDong from '../components/GuangDong'
-import { component } from 'vue/types/umd'
+import GuangDong from '../Pages/GuangDong'
 
 // 创建并暴露一个路由器对象
 export default new VueRouter({
