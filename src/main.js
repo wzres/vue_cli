95f5e4ae130b661dbc45f614b69cb9720fc0e221
wz <3565087505@qq.com>
2023-10-01 19:55:35 +0800

    初始化ElementUI

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import App from './App.vue'
 //引入VueRouter
 import VueRouter from 'vue-router'
 
+// 引入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 //关闭Vue的生产提示
 Vue.config.productionTip = false
 // 应用插件
 Vue.use(VueRouter)
+Vue.use(ElementUI);
 
 // 引用路由器对象
 import router from './router'
