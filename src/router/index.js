d6c8a94c80253c91a3846905c5d3529c5d543a52
wz <3565087505@qq.com>
2023-10-01 19:39:10 +0800

    变更

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 // 引入VueRouter
 import VueRouter from 'vue-router'
-// 引入组件
-import JiangXi from '../Pages/JiangXi'
-import GuangDong from '../Pages/GuangDong'
 
 // 创建并暴露一个路由器对象
 export default new VueRouter({
